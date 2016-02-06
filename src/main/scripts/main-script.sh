c2dc13360382fc2f3a0fddc1b194259800eb8ddb
nisargmodi <nisarg92modi@gmail.com>
2016-02-05 23:52:23 -0800

    edited main script

--- src/main/scripts/main-script.sh
+++ src/main/scripts/main-script.sh
@@ -5,9 +5,9 @@ echo $curr_path
 base_path=$curr_path/..
 echo $base_path
 
-path_to_tests=$base_path/resources/jsoup/target/
+path_to_tests=$base_path/resources/jsoup/target/classes
 
-export CLASSPATH=.:$base_path/resources/asm-all-5.0.4.jar:$base_path/resources/junit.jar:$base_path/java/instrumented/jsoup/target/classes
+CLASSPATH=.:$base_path/resources/asm-all-5.0.4.jar:$base_path/resources/junit.jar:$base_path/java/instrumented/jsoup/target/classes
 javac -cp $CLASSPATH $base_path/java/core-utils/Instrument.java
 
 echo "classpath is:"$CLASSPATH
@@ -16,27 +16,27 @@ cd $path_to_tests
 
 echo $(pwd)
 
-for f in $(find classes -name '*class')
+for f in $(find org -name '*class')
 do
-  mkdir -p $base_path/java/instrumented/jsoup/target/$(dirname $f)
+  mkdir -p $base_path/java/instrumented/jsoup/target/classes/$(dirname $f)
   cd $base_path/java/core-utils/
-  java -cp $CLASSPATH Instrument $path_to_tests/$f $base_path/java/instrumented/jsoup/target/$f
+  java -cp $CLASSPATH Instrument $path_to_tests/$f $base_path/java/instrumented/jsoup/target/classes/$f
   cd $path_to_tests
 done
 
 cp -R $base_path/resources/jsoup/target/classes/META-INF $base_path/java/instrumented/jsoup/target/classes/META-INF
 cp -R $base_path/resources/jsoup/target/test-classes $base_path/java/instrumented/jsoup/target/test-classes
 
-path_to_instrumented_tests=$base_path/java/instrumented/jsoup/target/test-classes
+path_to_instrumented_tests=$base_path/resources/jsoup/target/test-classes
 
 cd $path_to_instrumented_tests
 echo $(pwd)
 
-export CLASSPATH=.:$base_path/resources/asm-all-5.0.4.jar:$base_path/resources/junit.jar:$base_path/java/instrumented/jsoup/target/classes:$base_path/resources/jsoup/target/test-classes
+CLASSPATH=.:$base_path/resources/asm-all-5.0.4.jar:$base_path/resources/junit.jar:$base_path/java/instrumented/jsoup/target/classes:$base_path/resources/jsoup/target/test-classes
 
 for f in $(find org -name '*class')
 do
   arrIN=(${f//./ })
-  #echo $arrIN
+  echo $arrIN
   java -cp $CLASSPATH org.junit.runner.JUnitCore $arrIN
 done

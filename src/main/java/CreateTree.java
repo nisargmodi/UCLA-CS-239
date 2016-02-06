94b0df8d5ca8a43907cd33bc22291c2d609ba9ed
Punit Shetty <punitshetty30@gmail.com>
2016-02-06 00:06:19 -0800

    Modified CreateTree

--- src/main/java/CreateTree.java
+++ src/main/java/CreateTree.java
@@ -2,10 +2,12 @@ package main.java;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Stack;
 
@@ -18,11 +20,9 @@ public class CreateTree {
 	}
 
 	public void createTree() throws IOException {
-		BufferedReader input = new BufferedReader(
-				new FileReader("ENTER FILENAME HERE"));
-		BufferedWriter output = new BufferedWriter(
-				new FileWriter("ENTER FILENAME HERE"));
+		BufferedReader input = new BufferedReader(new FileReader("ENTER FILENAME HERE"));
 		Stack<String> stack = new Stack<String>();
+		ArrayList<String> list = new ArrayList<>();
 		String padding = "";
 
 		while (input.ready()) {
@@ -30,29 +30,24 @@ public class CreateTree {
 			if (stack.isEmpty() && line.contains("CALL")) {
 				stack.push("");
 				padding = padString();
-				output.write(padding + line);
-				output.newLine();
+				list.add(padding + line);
 				spaces++;
 			} else {
 				if (line.contains("CALL")) {
 					stack.push("");
 					padding = padString();
-					output.write(padding + line);
-					output.newLine();
+					list.add(padding + line);
 					spaces++;
 				}
 				if (line.contains("RETURN")) {
 					stack.pop();
 					spaces--;
-					padding = padString();
-					output.write(padding + line);
-					output.newLine();
+					list.add(padding + line);
 				}
 			}
 		}
-		System.out.println("Done");
 		input.close();
-		output.close();
+		this.createTreewithAnno(list);
 	}
 
 	public String padString() {
@@ -69,6 +64,26 @@ public class CreateTree {
 			return ins;
 		} else
 			return padding;
+	}
+
+	public void createTreewithAnno(ArrayList<String> list) throws IOException {
+		BufferedWriter output = new BufferedWriter(new FileWriter("ENTER FILENAME HERE"));
+
+		int count = 1;
+		for (int i = 2; i < list.size(); i++) {
+			if (list.get(i).contains("CALL") && list.get(i - 2).contains("CALL")) {
+				if (!list.get(i).equals(list.get(i - 2))) {
+					output.write(list.get(i) + "<COUNT>" + count);
+					output.newLine();
+					output.write(list.get(i));
+					output.newLine();
+					count = 1;
+				} else
+					count++;
+			}
+		}
+		System.out.println("CCT generated");
+		output.close();
 	}
 
 }
